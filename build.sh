ad73ee3f63839c00bf88acb1e4c0528637796a44
Jayson Fong <fong.jayson@gmail.com>
2025-01-03 13:24:56 -0500

    Adjustments for Wrangler watch

--- build.sh
+++ build.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
 TEMPLATE_FOLDER="public-templates"
+ASSETS_FOLDER="public-templates/assets"
 TARGET_FOLDER="public"
 
+asset_files=$(ls "$ASSETS_FOLDER")
 
-template_files=$(ls "$TEMPLATE_FOLDER")
+for asset_file in $asset_files; do
+  cp "$ASSETS_FOLDER/$asset_file" "$TARGET_FOLDER/$asset_file"
+done
+
+template_files=$(find "$TEMPLATE_FOLDER" -maxdepth 1 -type f)
 
 for template_file in $template_files; do
+  template_file=$(basename "$template_file")
   template_file_contents=$(cat "$TEMPLATE_FOLDER/$template_file")
 
   integrity_checks=$(grep -o -E "%%INTEGRITY:[a-zA-Z0-9_.-]+%%" "$TEMPLATE_FOLDER/$template_file")
   for integrity_check in $integrity_checks; do
     integrity_check_file_name=$(printf "%s" "$integrity_check" | cut -c "13-$((${#integrity_check}-2))")
 
-    integrity_sha512=$(openssl dgst -sha512 "$TARGET_FOLDER/$integrity_check_file_name" | tail -c 128 | xxd -r -p | base64)
+    integrity_sha512=$(openssl dgst -binary -sha512 "$ASSETS_FOLDER/$integrity_check_file_name" | base64)
     integrity_check_string="sha512-$integrity_sha512"
     template_file_contents=$(echo "$template_file_contents" | sed "s#$integrity_check#$integrity_check_string#")
   done
 
   echo "$template_file_contents" > "$TARGET_FOLDER/$template_file"
 done
+
+exit 0
